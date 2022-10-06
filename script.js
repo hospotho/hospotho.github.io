2e0b75655c674e6724c9facc264c743cb0214302
hospotho <a155268747@gmail.com>
2022-10-07 00:15:44 +0800

    update to v1

--- script.js
+++ script.js
@@ -8,165 +8,219 @@
   }
 
   function initCanvasBackground() {
-    const pixelSize = 5
-    const width = document.querySelector('section#top').clientWidth / pixelSize
-    const height = document.querySelector('section#top').clientHeight / pixelSize
-    const widthIndex = width + 1
-    const heightIndex = height * 2 + 1
-    const pRate = 0.5
-    const padding = 0
-
-    function createGraph(random = true) {
-      const array = []
-      const func = random ? Math.random : () => 0
-      for (let i = 0; i < heightIndex; i++) {
-        const subArray = []
-        for (let j = 0; j < widthIndex + (i % 2); j++) {
-          subArray.push(func())
+    function drawColorGraph(ctx, location = 0) {
+      function createColorizeGraph() {
+        function createRandomGraph() {
+          const array = new Array(heightIndex)
+          for (let i = 0; i < heightIndex; i += 2) {
+            const subArrayH = new Array(widthIndex)
+            for (let j = 0; j < widthIndex; j++) {
+              subArrayH[j] = Math.random()
+            }
+            const subArrayV = new Array(widthIndex + 1)
+            for (let j = 0; j < widthIndex + 1; j++) {
+              subArrayV[j] = Math.random()
+            }
+            array[i] = subArrayH
+            array[i + 1] = subArrayV
+          }
+          const subArrayH = new Array(widthIndex)
+          for (let j = 0; j < widthIndex; j++) {
+            subArrayH[j] = Math.random()
+          }
+          array[heightIndex] = subArrayH
+          return array
         }
-        array.push(subArray)
-      }
-      return array
-    }
+        function createEmptyGraph() {
+          const array = new Array(heightIndex)
+          for (let i = 0; i < heightIndex; i += 2) {
+            const subArrayH = new Array(widthIndex)
+            for (let j = 0; j < widthIndex; j++) {
+              subArrayH[j] = 0
+            }
+            const subArrayV = new Array(widthIndex + 1)
+            for (let j = 0; j < widthIndex + 1; j++) {
+              subArrayV[j] = 0
+            }
+            array[i] = subArrayH
+            array[i + 1] = subArrayV
+          }
+          const subArrayH = new Array(widthIndex)
+          for (let j = 0; j < widthIndex; j++) {
+            subArrayH[j] = 0
+          }
+          array[heightIndex] = subArrayH
+          return array
+        }
+        function visitNode(i, j, sub = false) {
+          if (visitedNode.has(a2s([i, j]))) return
+          visitedNode.add(a2s([i, j]))
+          unvisitedNode.delete(a2s([i, j]))
+          waitingNode.delete(a2s([i, j]))
+          if (graph[i][j] < pRate) return
 
-    function colorizeGraph(graph, coloredGraph) {
-      const a2s = arr => arr.toString()
-      const s2a = str => str.split(',').map(k => parseInt(k))
-      function visitNode(i, j, sub = false) {
-        if (visitedNode.has(a2s([i, j]))) return
-        visitedNode.add(a2s([i, j]))
-        unvisitedNode.delete(a2s([i, j]))
-        waitingNode.delete(a2s([i, j]))
-        if (graph[i][j] < pRate) return
+          coloredGraph[i][j] = count
 
-        coloredGraph[i][j] = count
+          if (i % 2) {
+            if (unvisitedNode.has(a2s([i - 2, j]))) {
+              waitingNode.add(a2s([i - 2, j]))
+            }
+            if (unvisitedNode.has(a2s([i - 1, j - 1]))) {
+              waitingNode.add(a2s([i - 1, j - 1]))
+            }
+            if (unvisitedNode.has(a2s([i - 1, j]))) {
+              waitingNode.add(a2s([i - 1, j]))
+            }
+            if (unvisitedNode.has(a2s([i + 1, j - 1]))) {
+              waitingNode.add(a2s([i + 1, j - 1]))
+            }
+            if (unvisitedNode.has(a2s([i + 1, j]))) {
+              waitingNode.add(a2s([i + 1, j]))
+            }
+            if (unvisitedNode.has(a2s([i + 2, j]))) {
+              waitingNode.add(a2s([i + 2, j]))
+            }
+          } else {
+            if (unvisitedNode.has(a2s([i - 1, j]))) {
+              waitingNode.add(a2s([i - 1, j]))
+            }
+            if (unvisitedNode.has(a2s([i - 1, j + 1]))) {
+              waitingNode.add(a2s([i - 1, j + 1]))
+            }
+            if (unvisitedNode.has(a2s([i, j - 1]))) {
+              waitingNode.add(a2s([i, j - 1]))
+            }
+            if (unvisitedNode.has(a2s([i, j + 1]))) {
+              waitingNode.add(a2s([i, j + 1]))
+            }
+            if (unvisitedNode.has(a2s([i + 1, j]))) {
+              waitingNode.add(a2s([i + 1, j]))
+            }
+            if (unvisitedNode.has(a2s([i + 1, j + 1]))) {
+              waitingNode.add(a2s([i + 1, j + 1]))
+            }
+          }
 
-        if (i % 2) {
-          if (unvisitedNode.has(a2s([i - 2, j]))) {
-            waitingNode.add(a2s([i - 2, j]))
+          while (!sub && waitingNode.size) {
+            const [newi, newj] = s2a(waitingNode.values().next().value)
+            visitNode(newi, newj, true)
           }
-          if (unvisitedNode.has(a2s([i - 1, j - 1]))) {
-            waitingNode.add(a2s([i - 1, j - 1]))
-          }
-          if (unvisitedNode.has(a2s([i - 1, j]))) {
-            waitingNode.add(a2s([i - 1, j]))
-          }
-          if (unvisitedNode.has(a2s([i + 1, j - 1]))) {
-            waitingNode.add(a2s([i + 1, j - 1]))
-          }
-          if (unvisitedNode.has(a2s([i + 1, j]))) {
-            waitingNode.add(a2s([i + 1, j]))
-          }
-          if (unvisitedNode.has(a2s([i + 2, j]))) {
-            waitingNode.add(a2s([i + 2, j]))
-          }
-        } else {
-          if (unvisitedNode.has(a2s([i - 1, j]))) {
-            waitingNode.add(a2s([i - 1, j]))
-          }
-          if (unvisitedNode.has(a2s([i - 1, j + 1]))) {
-            waitingNode.add(a2s([i - 1, j + 1]))
-          }
-          if (unvisitedNode.has(a2s([i, j - 1]))) {
-            waitingNode.add(a2s([i, j - 1]))
-          }
-          if (unvisitedNode.has(a2s([i, j + 1]))) {
-            waitingNode.add(a2s([i, j + 1]))
-          }
-          if (unvisitedNode.has(a2s([i + 1, j]))) {
-            waitingNode.add(a2s([i + 1, j]))
-          }
-          if (unvisitedNode.has(a2s([i + 1, j + 1]))) {
-            waitingNode.add(a2s([i + 1, j + 1]))
+        }
+        const a2s = arr => arr[0] * 10000 + arr[1]
+        const s2a = num => [(num - (num % 10000)) / 10000, num % 10000]
+
+        const graph = createRandomGraph()
+        const coloredGraph = createEmptyGraph()
+        const visitedNode = new Set()
+        const waitingNode = new Set()
+        const unvisitedNode = new Set()
+
+        for (let i = 0; i < heightIndex; i++) {
+          for (let j = 0; j < widthIndex + (i % 2); j++) {
+            unvisitedNode.add(a2s([i, j]))
           }
         }
 
-        while (!sub && waitingNode.size) {
-          const [newi, newj] = s2a(waitingNode.values().next().value)
-          visitNode(newi, newj, true)
+        let count = 1
+        while (unvisitedNode.size !== 0) {
+          const [i, j] = s2a(unvisitedNode.values().next().value)
+          const temp = visitedNode.size
+          visitNode(i, j)
+          if (visitedNode.size > temp + 1) count++
         }
-      }
 
-      const visitedNode = new Set()
-      const waitingNode = new Set()
-      const unvisitedNode = new Set()
-      for (let i = 0; i < heightIndex; i++) {
-        for (let j = 0; j < widthIndex + (i % 2); j++) {
-          unvisitedNode.add(a2s([i, j]))
+        return coloredGraph
+      }
+      function getGraphMax(arr) {
+        const array = arr.flat()
+        let max = array[0]
+        for (let i = 0; i < array.length; i++) {
+          max = array[i] > max ? array[i] : max
         }
+        return max
       }
+      const numToColor = num => '#' + Math.floor(16777215 - ((num * 0.9) / max) * 16777215).toString(16)
 
-      let count = 1
-      while (unvisitedNode.size !== 0) {
-        const [i, j] = s2a(unvisitedNode.values().next().value)
-        const temp = visitedNode.size
-        visitNode(i, j)
-        if (visitedNode.size > temp + 1) count++
-      }
-    }
+      const coloredGraph = createColorizeGraph()
+      const max = getGraphMax(coloredGraph)
+      const left = bodyWidth * location + padding
 
-    // function drawMonoGraph(graph, ctx) {
-    //   ctx.beginPath()
-    //   for (let i = 0; i < heightIndex; i++) {
-    //     for (let j = 0; j < widthIndex + (i % 2); j++) {
-    //       if (graph[i][j] > pRate) {
-    //         const baseX = j * pixelSize + padding
-    //         const baseY = (i - (i % 2) - parseInt(i / 2)) * pixelSize + padding
-    //         ctx.moveTo(baseX, baseY)
-    //         i % 2 ? ctx.lineTo(baseX, baseY + pixelSize) : ctx.lineTo(baseX + pixelSize, baseY)
-    //       }
-    //     }
-    //   }
-    //   ctx.stroke()
-    // }
+      ctx.clearRect(left, 0, bodyWidth, bodyHeight)
 
-    function drawColorGraph(coloredGraph, ctx) {
-      const max = Math.max(...[...new Set(coloredGraph.flat())])
-      for (let i = 0; i < heightIndex; i++) {
-        for (let j = 0; j < widthIndex + (i % 2); j++) {
+      for (let i = 0; i < heightIndex; i += 2) {
+        const baseY = (i / 2) * pixelSize + padding
+        for (let j = 0; j < widthIndex; j++) {
           if (coloredGraph[i][j] === 0) continue
           ctx.beginPath()
-          ctx.strokeStyle = '#' + Math.floor(16777215 - ((coloredGraph[i][j] * 0.9) / max) * 16777215).toString(16)
-          const baseX = j * pixelSize + padding
-          const baseY = (i - (i % 2) - parseInt(i / 2)) * pixelSize + padding
+          ctx.strokeStyle = numToColor(coloredGraph[i][j])
+          const baseX = left + j * pixelSize
           ctx.moveTo(baseX, baseY)
-          i % 2 ? ctx.lineTo(baseX, baseY + pixelSize) : ctx.lineTo(baseX + pixelSize, baseY)
+          ctx.lineTo(baseX + pixelSize, baseY)
+          ctx.stroke()
+        }
+        for (let j = 0; j < widthIndex + 1; j++) {
+          if (coloredGraph[i + 1][j] === 0) continue
+          ctx.beginPath()
+          ctx.strokeStyle = numToColor(coloredGraph[i + 1][j])
+          const baseX = left + j * pixelSize
+          ctx.moveTo(baseX, baseY)
+          ctx.lineTo(baseX, baseY + pixelSize)
           ctx.stroke()
         }
       }
-    }
-
-    function initCanvas(canvas) {
-      const ctx = canvas.getContext('2d')
-      ctx.canvas.width = width * pixelSize + padding * 2
-      ctx.canvas.height = height * pixelSize + padding * 2
-
-      const graph = createGraph()
-      const coloredGraph = createGraph(false)
-      colorizeGraph(graph, coloredGraph)
-      // drawMonoGraph(graph, ctx)
-      drawColorGraph(coloredGraph, ctx)
-    }
-
-    const canvas1 = document.querySelector('canvas#background1')
-    const canvas2 = document.querySelector('canvas#background2')
-    const canvas3 = document.querySelector('canvas#background3')
-
-    initCanvas(canvas1)
-
-    clearInterval(canvasInterval)
-    canvasInterval = setInterval(() => {
-      const left = window.getComputedStyle(canvas1).left.slice(0, -2)
-      const right = window.getComputedStyle(canvas3).left.slice(0, -2)
-      if (left === '0') {
-        initCanvas(canvas2)
-        initCanvas(canvas3)
+      const baseY = (heightIndex / 2) * pixelSize + padding
+      for (let j = 0; j < widthIndex; j++) {
+        if (coloredGraph[heightIndex][j] === 0) continue
+        ctx.beginPath()
+        ctx.strokeStyle = numToColor(coloredGraph[heightIndex][j])
+        const baseX = left + j * pixelSize
+        ctx.moveTo(baseX, baseY)
+        ctx.lineTo(baseX + pixelSize, baseY)
+        ctx.stroke()
       }
-      if (right === '0') {
-        initCanvas(canvas1)
-        initCanvas(canvas2)
+    }
+    
+    const pixelSize = 10
+    const padding = 20
+    const pRate = 0.5
+
+    const bodyWidth = document.documentElement.clientWidth
+    const bodyHeight = document.documentElement.clientHeight
+    const containerWidth = document.querySelector('section#top').clientWidth
+
+    const canvasWidth = containerWidth - padding * 2
+    const canvasHeight = bodyHeight - padding * 2
+    const canvasWidthPixels = canvasWidth / pixelSize
+    const canvasHeightPixels = canvasHeight / pixelSize
+
+    const widthIndex = Math.ceil(canvasWidthPixels)
+    const heightIndex = Math.ceil(canvasHeightPixels * 2)
+
+
+
+    const canvas = document.querySelector('canvas#background')
+    const ctx = canvas.getContext('2d')
+    ctx.canvas.width = bodyWidth * 3
+    ctx.canvas.height = bodyHeight
+
+    drawColorGraph(ctx)
+    drawColorGraph(ctx, 1)
+    drawColorGraph(ctx, 2)
+
+    canvas.style = 'animation: loop ease 16s infinite alternate;'
+
+    const loop = () => {
+      const transform = window.getComputedStyle(canvas).transform
+      if (transform === 'matrix(1, 0, 0, 1, 0, 0)') {
+        drawColorGraph(ctx, 1)
+        drawColorGraph(ctx, 2)
+      } else {
+        drawColorGraph(ctx)
+        drawColorGraph(ctx, 1)
       }
-    }, 1000)
+    }
+    canvas.removeEventListener('animationiteration', loop)
+    canvas.addEventListener('animationiteration', loop)
   }
 
   async function initProjectDiv() {
@@ -209,7 +263,7 @@
 
   function initSoupDiv() {
     const folder = 'chickenSoup/'
-    const imgURL = ['de.jpg', 'mo.jpg', 'sea.jpg', 'sno.jpg', 'sta.jpg']
+    const imgURL = ['de-small.jpg', 'mo-small.jpg', 'sea-small.jpg', 'sno-small.jpg', 'sta-small.jpg']
     const quotes = [
       '“Perfection has to do with the end product, but excellence has to do with the process.” – Jerry Moran',
       '“Excellence is a continuous process and not an accident.” – A. P. J. Abdul Kalam',
@@ -225,12 +279,29 @@
       '“Big dreams create the magic that stir men’s souls to greatness.” — Bill McCartney'
     ]
 
-    const img = document.querySelector('img#chickenSoupImg')
-    const wrapper = document.querySelector('section#chickenSoup .wrapper')
+    const wrapper = document.querySelector('section#chickenSoup .chickenSoupFrame')
+    const span = wrapper.querySelector('span#chickenSoupCation')
     const button = document.querySelector('button#refresh')
-    wrapper.width = img.clientWidth + 'px'
-    wrapper.height = img.clientHeight + 'px'
-    wrapper.style = `left: ${(0.5 - img.clientWidth / window.clientWidth / 2) * 100}%;`
+
+    let imgIndex = 0
+    let quoteIndex = 0
+
+    const randomIndex = (length, curr) => {
+      let newIndex = Math.floor(length * Math.random())
+      while (newIndex === curr) {
+        newIndex = Math.floor(length * Math.random())
+      }
+      return newIndex
+    }
+
+    const action = () => {
+      imgIndex = randomIndex(imgURL.length, imgIndex)
+      quoteIndex = randomIndex(quotes.length, quoteIndex)
+      wrapper.style = `background-image: url("${folder + imgURL[imgIndex]}");`
+      span.innerHTML = quotes[quoteIndex]
+    }
+    button.removeEventListener('click', action)
+    button.addEventListener('click', action)
   }
 
   function init() {
@@ -238,11 +309,12 @@
     initCanvasBackground()
     initProjectDiv()
     initSoupDiv()
-    // initDivTransition()
   }
 
-  let canvasInterval
-
+  let debounceTimeout
+  window.addEventListener('resize', () => {
+    clearTimeout(debounceTimeout)
+    debounceTimeout = setTimeout(init, 500)
+  })
   init()
-  window.addEventListener('resize', init)
 })()
