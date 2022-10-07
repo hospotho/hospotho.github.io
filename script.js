9f3a0373f6ae8e0524359c9b5259501580e0dc00
hospotho <a155268747@gmail.com>
2022-10-07 14:56:32 +0800

    fix formatDate()

--- script.js
+++ script.js
@@ -230,7 +230,7 @@
       const d = new Date(date)
       const year = d.getFullYear()
       const month = (d.getMonth() + 1).toString().padStart(2, '0')
-      const day = d.getMonth().toString().padStart(2, '0')
+      const day = d.getDate().toString().padStart(2, '0')
       return `${year}/${month}/${day}`
     }
 
